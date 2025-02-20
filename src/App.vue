e385fdd7b3ff939c13d9aeb3b160fc581da54407
MemCh1kk <memetov.k.i.23@gmail.com>
2025-02-20 12:34:31 +0300

    task 1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+	export default{ data() {
+	return {
+		text1: 'Vlad',
+		text2: 'pidoras',
+	}
+    }
+}
+</script>
+<template>
+    <div>{{ text1 }}</div>
+    <div>{{ text2 }}</div>
+</template>
