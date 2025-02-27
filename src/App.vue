280552d4a547a8357d48c073df91ae946f77370f
MemCh1kk <memetov.k.i.23@gmail.com>
2025-02-27 11:19:00 +0300

    task 3.2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 	export default{ data() {
 	return {
-		num: 5,
+		num1: 1,
+		num2: 2,
+		num3: 3,
 	}
     }
 }
 </script>
 <template>
-    {{ num }}
+    {{ num1 + num2 + num3 }}
 </template>
